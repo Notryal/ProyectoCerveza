418f4977c30358426e8d68746db074be2ce5ce13
mbelenm <machenabelen@gmail.com>
2022-10-16 01:24:29 -0300

    acodarError

--- js/script.js
+++ js/script.js
@@ -1,27 +1,16 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
-//   $('.color-choose input').on('click', function() {
-//       var headphonesColor = $(this).attr('data-image');
+  $('.color-choose input').on('click', function() {
+      var headphonesColor = $(this).attr('data-image');
 
-//       $('.active').removeClass('active');
-//       $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
-//       $(this).addClass('active');
-//   });
+      $('.active').removeClass('active');
+      $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+      $(this).addClass('active');
+  })
 
-<<<<<<< HEAD
+
 });
 
-
-//Nosotros
-
-function classToggle() {
-  var el = document.querySelector('.icon-cards__content');
-  el.classList.toggle('step-animation');
-}
-
-document.querySelector('#toggle-animation').addEventListener('click', classToggle);
-=======
-// });
 
 // Producto h1
     function Golden(){
@@ -59,4 +48,4 @@ document.querySelector('#toggle-animation').addEventListener('click', classToggl
       document.getElementById('description')
       .innerHTML="Cerveza oscura profunda, con un rojo intenso a trasluz. Carácter tostado y notas a chocolate. Sabores a malta,  bizcochos y pan tostado. Cuerpo medio alto, amargor medio con notas terrosas.";
     }  
->>>>>>> f7df5377b73c625febddeed9df26f89aa55dea96
+
